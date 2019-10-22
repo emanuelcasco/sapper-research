0f22f496ea9ccf235a4d16aa74a41b5931d4c546
Emanuel Casco <ecasco@woloxs-MacBook-Pro.local>
2019-10-22 16:08:02 -0300

    Kickoff

---
+++ src/routes/index.svelte
@@ -0,0 +1,52 @@
+<style>
+	h1, h2, figure, p {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	h2 {
+		font-size: 2.0em;
+		margin: 0 0 0.5em 0;
+	}
+
+	figure {
+		margin: 0 0 1em 0;
+	}
+
+	img {
+		width: 100%;
+		max-width: 300px;
+		margin: 0 0 1em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>helm</title>
+</svelte:head>
+
+<h1>helm</h1>
+<h2>Emanuel Casco</h2>
+
+<figure>
+	<img alt='Helmy' src='helmet.png'>
+	<figcaption>HIGH FIVE!</figcaption>
+</figure>
+
+<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
